88b9cdc290e02a8a6544f4348f52e9e2764db3bb
Mathias Rav <rav@cs.au.dk>
2017-08-27 18:16:26 +0200

    Call clean.sh

--- sqldump.sh
+++ sqldump.sh
@@ -4,6 +4,8 @@ function db_dump_base() {
 	dbpass="$3"
 	filename="$project`date +%Y%m%d%H%M`.sql"
 	umask 077 ; mysqldump -u"$dbuser" -p"$dbpass" --databases "$project" > ~/sqldump/"$filename" ; umask 022
+	(cd ~/sqldump/$project &&
+	 [ -x clean.sh ] && ./clean.sh)
 	(cd ~/sqldump/$project/.git &&
 	 cd .. &&
 	 git diff --quiet &&
