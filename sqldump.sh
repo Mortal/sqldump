0d1fc819b2e4ba3d8056c1fd95c3c40ba953f6f0
Mathias Rav <rav@cs.au.dk>
2017-09-18 14:15:11 +0200

    Fix call to clean.sh

--- sqldump.sh
+++ sqldump.sh
@@ -4,12 +4,11 @@ function db_dump_base() {
 	dbpass="$3"
 	filename="$project`date +%Y%m%d%H%M`.sql"
 	umask 077 ; mysqldump -u"$dbuser" -p"$dbpass" --databases "$project" > ~/sqldump/"$filename" ; umask 022
-	(cd ~/sqldump/$project &&
-	 [ -x clean.sh ] && ./clean.sh)
 	(cd ~/sqldump/$project/.git &&
 	 cd .. &&
 	 git diff --quiet &&
 	 mv "../$filename" "$project.sql" &&
-	 git add "$project.sql" &&
-	 git commit -qm "Update $project.sql")
+	 ([ -x clean.sh ] && ./clean.sh;
+	  git add "$project.sql" &&
+	  git commit -qm "Update $project.sql")) || echo "sqldump.sh: Failed to commit"
 }
